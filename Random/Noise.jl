4ca724ed4a74b48c4c8b0bd8e5e875b62f17fb2f
“Jay <jay@kickliter.com>
2014-03-11 20:54:39 -0600

    Added Noise.jl. This will contain functions for creating and adding white gaussian noise.

---
+++ Random/Noise.jl
@@ -0,0 +1,17 @@
+function wgn(length::Integer, power::Real=1.0, impedence::Real=1.0, units::String = "linear", returnComplex::Bool=false)
+    if units == "linear"
+        np = power
+    elseif units == "dBm"
+        np = 10^((power - 30)/10)
+    elseif units == "dBW"
+        np = 10^(power/10)
+    end
+
+    if returnComplex
+        noiseVec = sqrt(impedence*np/2) * ( randn(length) + randn(length)im )
+    else
+        noiseVec = sqrt(impedence*np/2) * randn(length)
+    end
+
+    return noiseVec
+end
