971ba1f43526d6b6cbb0b70b1357000b0c319527
Jay Kickliter <jay@kickliter.com>
2014-03-17 12:27:14 -0600

    Added rms function for complex inputs

--- src/Math/Power.jl
+++ src/Math/Power.jl
@@ -5,3 +5,11 @@ function rms{T<:Real}(s::Array{T,1})
    end
    sqrt(m/length(s))
 end
+
+function rms{T<:Real}(s::Array{Complex{T},1})
+   m = zero(T)
+   for si in s
+       m += abs2(si)
+   end
+   sqrt(m/length(s))
+end
