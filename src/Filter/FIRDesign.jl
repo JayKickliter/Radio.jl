5b25acc5d97b589c0c0e2a8b2084a91a5f04a89e
“Jay <jay@kickliter.com>
2014-03-19 10:02:48 -0600

    Modified firdes to conform to conventions. M is the filter order, but the function returns M+1 impulse samples.

--- src/Filter/FIRDesign.jl
+++ src/Filter/FIRDesign.jl
@@ -3,7 +3,7 @@ export firdes
 #==============================================================================#
 #                                Windowed Filter                               #
 #==============================================================================#
-# M      = Filter order: number of impulse response samples to return
+# M      = Filter order; Returns M+1 samples
 # F_t    = Normalized transition frequency. CutoffFreq/SampleRate
 # window = Window function to apply to ideal truncated sinc response
 function firdes( M::Integer, F_t::Real, windowFunction::Function )
@@ -11,10 +11,10 @@ function firdes( M::Integer, F_t::Real, windowFunction::Function )
         error("F_t must be greater than 0 and less than 1")
     end
     
-    H_t = windowFunction( M )
+    H_t = windowFunction( M+1 )
     
-    for n in 1:M
-        H_t[n] = sinc(2*π*F_t*(n-M/2)) * H_t[n]
+    for n in 0:M
+        H_t[n+1] = 2*F_t*sinc(2*F_t*(n-M/2)) * H_t[n+1]
     end
     
     return H_t
