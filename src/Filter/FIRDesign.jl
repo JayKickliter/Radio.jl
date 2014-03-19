0b181c45ce1fc0f3bb6fd58abc51006f688cf413
“Jay <jay@kickliter.com>
2014-03-18 20:16:12 -0600

    added rectangular window and sorted functions into alphabetical order

---
+++ src/Filter/FIRDesign.jl
@@ -0,0 +1,24 @@
+export firdes
+
+#==============================================================================#
+#                                Windowed Filter                               #
+#==============================================================================#
+# M      = Filter order: number of impulse response samples to return
+# F_t    = Normalized transition frequency. CutoffFreq/SampleRate
+# window = Window function to apply to ideal truncated sinc response
+function firdes( M::Integer, F_t::Real, windowFunction::Function )
+    if F_t > 0 && F_t < 1
+        error("F_t must be greater than 0 and less than 1")
+    end
+    if !method_exists( windowFunction, ())
+        error("Specified window doesn't exist. See Window.jl for valid windows")
+    end
+    
+    H_t = windowFunction( M )
+    
+    for n in 1:M
+        H_t[n] = sinc(2*π*F_t*(n-M/2)) * H_t[n]
+    end
+    
+    return H_t
+end
