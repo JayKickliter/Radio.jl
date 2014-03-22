f722e1df5480cadda4a9c0ca58b1591cac3f3dea
“Jay <jay@kickliter.com>
2014-03-22 14:42:02 -0600

    fixed range check for F_t

--- src/Filter/FIRDesign.jl
+++ src/Filter/FIRDesign.jl
@@ -8,18 +8,12 @@ export firdes
 # M      = Filter order; Returns M+1 samples
 # F_t    = Normalized transition frequency. CutoffFreq/SampleRate
 # window = Window function to apply to ideal truncated sinc response
-function firdes( M::Integer, F_t::Real, windowFunction::Function )
-    if F_t < 0 && F_t > 1
-        error("F_t must be greater than 0 and less than 1")
+function firdes( M::Integer, F_t::FloatingPoint, windowFunction::Function )
+    if F_t < 0 && F_t > 0.5
+        error("F_t must be > 0.0 and < 0.5")
     end
     
-    H_t = Array(Float64, M+1)
-    
-    for n in 0:M
-        H_t[n+1] = 2*F_t*sinc(2*F_t*(n-M/2)) * windowFunction(n+1, M+1)
-    end
-    
-    return H_t
+    [ 2*F_t*sinc(2*F_t*(n-M/2)) * windowFunction(n+1, M+1) for n = 0:M ]
 end
 
 end # module Filter
