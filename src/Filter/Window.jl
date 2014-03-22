e873670ff432364b941e38c7b3f09b6fd13fba9c
“Jay <jay@kickliter.com>
2014-03-22 09:39:13 -0600

    Made scalar versions of all the window functions. This will allow better integration of the window functions in a devectorized loop.

--- src/Filter/Window.jl
+++ src/Filter/Window.jl
@@ -4,6 +4,12 @@ export blackaman, hamming, hann, kaiser, rectangular
 #                                Blackman Window                               #
 #==============================================================================#
 
+function blackaman( n::Integer, N::Integer )
+    α = 0.42
+    β = 0.5
+    α - β*cos(2*π*n/(N-1)) + (β-α)*cos(4*π*n/(N-1))
+end
+
 function blackaman( N::Integer )
     n = 0:N-1
     α = 0.42
@@ -14,6 +20,12 @@ end
 #==============================================================================#
 #                                Hamming Window                                #
 #==============================================================================#
+
+function hamming( n::Integer, N::Integer )
+    α = 0.54
+    β = 0.46 # 1 - α
+    α - β*cos(2*π*n/(N-1))
+end
 
 function hamming( N::Integer )
     n = 0:N-1
@@ -26,6 +38,12 @@ end
 #                                  Hann Window                                 #
 #==============================================================================#
 
+function hann( n::Integer, N::Integer )
+    α = 0.5
+    β = 0.5
+    α - β*cos(2*π*n/(N-1))
+end
+
 function hann( N::Integer )
     n = 0:N-1
     α = 0.5
@@ -37,9 +55,16 @@ end
 #                                Kaiser Window                                 #
 #==============================================================================#
 
-function kaiser( N::Integer, α::Real )
+function kaiser( n::Integer, N::Integer, α::Real )
     β   = π*α
+    num = besseli(0, β*sqrt(1-(2*n/(N-1)-1).^2))
+    dem = besseli(0, β)
+    num/dem
+end
+
+function kaiser( N::Integer, α::Real )
     n   = 0:N-1
+    β   = π*α
     num = besseli(0, β*sqrt(1-(2*n/(N-1)-1).^2))
     dem = besseli(0, β)
     num/dem
@@ -49,6 +74,10 @@ end
 #                              Rectangular Window                              #
 #==============================================================================#
 
+function rectangular( n::Integer, N::Integer )
+    1.0
+end
+
 function rectangular( N::Integer )
-    ones( Float64, N )
+    ones( typeof(1.0), N )
 end
