853407c8d5798067f8c3ffd4f3f84a539ad5bb8a
“Jay <jay@kickliter.com>
2014-03-22 14:34:24 -0600

    Wrapped PSK.jl in module Modulation

--- src/Modulation/PSK.jl
+++ src/Modulation/PSK.jl
@@ -1,3 +1,7 @@
+module Modulation
+
+export pskmod
+
 function pskmod( data, M::Integer, encoding::String )
     m = [ 0 : M-1 ]
     if M == 4
@@ -19,3 +23,5 @@ function pskmod( M::Integer, length::Integer )
     data = rand( 0:M-1, length )
     pskmod( data, M, "" )
 end
+
+end # module Modulation
