5d5d5ced2cbdbee5eac8c5bb7fd320aa7d1fe0df
Jay Kickliter <jay@kickliter.com>
2014-03-27 09:45:43 -0600

    Modified plot_response to show both the impulse and frequency response of a fir filter

--- src/Support/Graphics.jl
+++ src/Support/Graphics.jl
@@ -7,10 +7,31 @@ function plot_response( x )
     if !method_exists( plot, ())
         error( "To use plot_response, you must load the Winston package")
     end
-    
-    X = fft(x)
+    M = [0:length(x) - 1]
+    n = int(ceil(length( x )/2))
+    X = fft(x)[1:n]
     X = abs(X)
     X = 20*log10( X )
-    f = linspace( -0.5, 0.5, length(X) )
-    display( plot(f, X) )
+    f = linspace( 0, 1, n )
+    
+    impulse = FramedPlot( 
+                            title  = "Impulse Response",
+                            xlabel = "Sample #",
+                            ylabel = "Amplitude"
+                        )
+    add(impulse, Curve(M, x))                    
+    add(impulse, Points(M, x, kind="filled circle"))
+    
+    freq = FramedPlot( 
+                            title  = "Frequency Response",
+                            xlabel = "Normalized Frequency",
+                            ylabel = "Decibels"
+                        )
+    add(freq, Curve(f, X))                    
+    add(freq, Points(f, X, kind="filled circle"))
+    
+    t = Table(2, 1)
+    t[1,1] = impulse
+    t[2,1] = freq
+    display(t)
 end
