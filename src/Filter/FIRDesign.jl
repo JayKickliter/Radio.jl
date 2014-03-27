ecd52acde4b7804e56e102886c34068046abc2bd
Jay Kickliter <jay@kickliter.com>
2014-03-27 09:50:17 -0600

    made some changes to match the conventions in Discrete Time Signal Processing by Oppenheim.

--- src/Filter/FIRDesign.jl
+++ src/Filter/FIRDesign.jl
@@ -4,19 +4,33 @@
 # M      = Filter order; Returns M+1 samples
 # F_t    = Normalized transition frequency. CutoffFreq/SampleRate
 # window = Window function to apply to ideal truncated sinc response
-function firdes( M::Integer, F_t::FloatingPoint, windowFunction::Function )
+function firdes( M::Integer, F_t::Real, windowFunction::Function )
     if F_t < 0 && F_t > 0.5
         error("F_t must be > 0.0 and < 0.5")
     end
     
-    [ 2*F_t*sinc(2*F_t*(n-M/2)) * windowFunction(n+1, M+1) for n = 0:M ]
+    [ 2*F_t*sinc(2*F_t*(n-M/2)) * windowFunction(n, M) for n = 0:M ]
 end
+
+# F_t    = Normalized transition frequency. CutoffFreq/SampleRate
+# window = A vector containging pre-calculated window coefficients
+function firdes( F_t::Real, window::Vector )
+    if F_t < 0 && F_t > 0.5
+        error("F_t must be > 0.0 and < 0.5")
+    end
+    
+    M = length( window ) - 1
+    
+    [ 2*F_t*sinc(2*F_t*(n-M/2)) * window[n+1] for n = 0:M ]
+end
+
 
 #==============================================================================#
 #                                Kaiser Order                                  #
 #==============================================================================#
 # δ  = ripple in passband and stopband
-# Δω = transition width
+# Δω = ω_p - ω_s
+#    = transition width in radians 
 # 
 # returns:
 #   M = approxomate filter order,
@@ -24,14 +38,13 @@ end
 function kaiserord( δ::Real, Δω )    
     A = -20*log10( δ )
     if A > 50
-        α = 0.1102*( A - 8.7 )
+        β = 0.1102*( A - 8.7 )
     elseif A >= 21
-        α = 0.5842*( A - 21 )^( 0.4 ) + 0.07886*( A - 21 )
+        β = 0.5842*( A - 21 )^( 0.4 ) + 0.07886*( A - 21 )
     else
-        α = 0
+        β = 0.0
     end
-    M = ( A - 7.95 )/( 2.285 * Δω )
-    M = int(ceil(M))
-    
-    return ( M, α )
+    M = int(ceil( ( A - 7.95 )/( 2.285 * Δω )))
+
+    return ( M, β )
 end
