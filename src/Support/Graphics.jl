d122c428e4a2956c3fbad4c42c369413935bce1e
Jay Kickliter <jay@kickliter.com>
2014-03-27 12:18:26 -0600

    Added zero padding to plot_response's frequency domain plot. This makes it possible to actually see the side lobes.

--- src/Support/Graphics.jl
+++ src/Support/Graphics.jl
@@ -1,3 +1,6 @@
+using Winston
+
+
 #==============================================================================#
 #                           Plot Frequency Response                            #
 #==============================================================================#
@@ -11,19 +14,20 @@ function plot_response( x )
     if !method_exists( plot, ())
         error( "To use plot_response, you must load the Winston package")
     end
-    M = [0:length(x) - 1]
-    n = int(ceil(length( x )/2))
-    X = fft(x)[1:n]
-    X = abs(X)
-    X = 20*log10( X )
-    f = linspace( 0, 1, n )
+    xx = [x, zeros(1024 - length(x))]
+    M  = [0:length(x) - 1]
+    n  = int(ceil(length( x )/2))
+    X  = fft(xx)[1:512]
+    X  = abs(X)
+    X  = 20*log10( X )
+    f  = linspace( 0, 1, 512 )
     
     impulse = FramedPlot( 
                             title  = "Impulse Response",
                             xlabel = "Sample #",
                             ylabel = "Amplitude"
                         )
-    add(impulse, Curve(M, x))                    
+    # add(impulse, Curve(M, x))                    
     add(impulse, Points(M, x, kind="filled circle"))
     
     freq = FramedPlot( 
@@ -32,7 +36,7 @@ function plot_response( x )
                             ylabel = "Decibels"
                         )
     add(freq, Curve(f, X))                    
-    add(freq, Points(f, X, kind="filled circle"))
+    # add(freq, Points(f, X, kind="filled circle"))
     
     t = Table(2, 1)
     t[1,1] = impulse
