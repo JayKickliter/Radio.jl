dc9a37bf1543436a8f4daeaf24a87df8e01f6d8e
Jay Kickliter <jay@kickliter.com>
2014-03-27 09:51:05 -0600

    Now all windows take the parameter M, instead of N. M is the filter order, the number of points returned are M+1

--- src/Filter/Window.jl
+++ src/Filter/Window.jl
@@ -2,67 +2,66 @@
 #                                Blackman Window                               #
 #==============================================================================#
 
-function blackaman( n::Integer, N::Integer )
+function blackaman( n::Integer, M::Integer )
     α = 0.42
     β = 0.5
-    α - β*cos(2*π*n/(N-1)) + (β-α)*cos(4*π*n/(N-1))
+    α - β*cos(2*π*n/M) + (β-α)*cos(4*π*n/M)
 end
 
-function blackaman( N::Integer )
-    [ blackaman(n, N) for n = 0:N-1 ]
+function blackaman( M::Integer )
+    [ blackaman(n, M) for n = 0:M ]
 end
 
 #==============================================================================#
 #                                Hamming Window                                #
 #==============================================================================#
 
-function hamming( n::Integer, N::Integer )
+function hamming( n::Integer, M::Integer )
     α = 0.54
     β = 0.46 # 1 - α
-    α - β*cos(2*π*n/(N-1))
+    α - β*cos(2*π*n/M)
 end
 
-function hamming( N::Integer )
-    [ hamming(n, N) for n = 0:N-1 ]
+function hamming( M::Integer )
+    [ hamming(n, M) for n = 0:M ]
 end
 
 #==============================================================================# 
 #                                  Hann Window                                 #
 #==============================================================================#
 
-function hann( n::Integer, N::Integer )
+function hann( n::Integer, M::Integer )
     α = 0.5
     β = 0.5
-    α - β*cos(2*π*n/(N-1))
+    α - β*cos(2*π*n/M)
 end
 
-function hann( N::Integer )
-    [ hann(n, N) for n = 0:N-1 ]
+function hann( M::Integer )
+    [ hann(n, M) for n = 0:M ]
 end
 
 #==============================================================================#
 #                                Kaiser Window                                 #
 #==============================================================================#
 
-function kaiser( n::Integer, N::Integer, α::Real )
-    β   = π*α
-    num = besseli(0, β*sqrt(1-(2*n/(N-1)-1).^2))
+function kaiser( n::Integer, M::Integer, β::Real )
+    num = besseli(0, β*sqrt(1-(2*n/M-1).^2))
     dem = besseli(0, β)
     num/dem
 end
 
-function kaiser( N::Integer, α::Real )
-    [ kaiser( n, N, α ) for n = 0:N-1 ]
+function kaiser( M::Integer, β::Real )
+    [ kaiser( n, M, β ) for n = 0:M ]
 end
 
 #==============================================================================#
 #                              Rectangular Window                              #
 #==============================================================================#
 
-function rectangle( n::Integer, N::Integer )
+function rectangle( n::Integer, M::Integer )
     1.0
 end
 
-function rectangle( N::Integer )
-    ones( typeof(1.0), N )
+function rectangle( M::Integer )
+    ones( typeof(1.0), M )
 end
