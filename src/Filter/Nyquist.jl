fb7d437c8dca01c99a79220b4214b5470903bd6c
Jay Kickliter <jay@kickliter.com>
2014-03-29 10:12:54 -0600

    changed α to β

--- src/Filter/Nyquist.jl
+++ src/Filter/Nyquist.jl
@@ -1,21 +1,21 @@
 #==============================================================================#
 #                                Raised Cosine                                 #
 #==============================================================================#
-# α    = Rolloff factor
+# β    = Rolloff factor
 # span = filter span. How many symbols the filter should cover
 # sps  = Samples per symbol
 
-function rcos( α, span, sps )
+function rcos( β, span, sps )
     hlen = sps*span
     hlen = iseven(hlen) ? hlen+1 : hlen
     h    = Array( Float64, hlen )
     
     for i = 1:hlen
         t = i - 1 - (hlen - 1)/2
-        if abs(α*t/sps) == 0.5      # check for case where (1-(2*α*t/sps)^2 is zero, which would cause divide by zero
+        if abs(β*t/sps) == 0.5      # check for case where (1-(2*β*t/sps)^2 is zero, which would cause divide by zero
             h[i] = 0.5
         else
-            h[i] = sinc(t/sps)*cos(π*α*t/sps)/(1-(2*α*t/sps)^2)
+            h[i] = sinc(t/sps)*cos(π*β*t/sps)/(1-(2*β*t/sps)^2)
         end
     end 
 
@@ -25,11 +25,11 @@ end
 #==============================================================================#
 #                             Root Raised Cosine                               #
 #==============================================================================#
-# α    = Rolloff factor
+# β    = Rolloff factor
 # span = filter span. How many symbols the filter should cover
 # sps  = Samples per symbol
 
-function rrcos( α, span, sps )
+function rrcos( β, span, sps )
     hlen = sps*span
     hlen = iseven(hlen) ? hlen+1 : hlen
     h    = Array( Float64, hlen )
@@ -37,13 +37,13 @@ function rrcos( α, span, sps )
     for i = 1:hlen
         t = i - (hlen - 1)/2 - 1 
         if t == 0                   # full equation is undefined at t = 0
-            h[i] = (1/sqrt(sps)) * (1+α*(4/π -1))  
-        elseif abs(t) == sps/(4*α)  # another case when full equation is undefined
-            h[i] = (α/sqrt(2*sps)) * ( (1+(2/π))*sin(π/(4*α)) + (1-(2/π))*cos(π/(4*α)) ) 
+            h[i] = (1/sqrt(sps)) * (1+β*(4/π -1))  
+        elseif abs(t) == sps/(4*β)  # another case when full equation is undefined
+            h[i] = (β/sqrt(2*sps)) * ( (1+(2/π))*sin(π/(4*β)) + (1-(2/π))*cos(π/(4*β)) ) 
         else
-            h[i] = (4*α)/(π*sqrt(sps)) * ( cos((1+α)*π*t/sps) + (sps/(4*α*t))*sin((1-α)*π*t/sps) ) / (1-(4*α*t/sps)^2)
+            h[i] = (4*β)/(π*sqrt(sps)) * ( cos((1+β)*π*t/sps) + (sps/(4*β*t))*sin((1-β)*π*t/sps) ) / (1-(4*β*t/sps)^2)
         end
     end
-
+  
     return h
 end
