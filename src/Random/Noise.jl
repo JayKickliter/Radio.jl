66b0517bc565c7d0953df96767b5b266d0c65a31
Jay Kickliter <jay@kickliter.com>
2014-04-02 10:07:03 -0600

    Fixed wgn. Made units parameter case-insensitive. Added unit "volt".

--- src/Random/Noise.jl
+++ src/Random/Noise.jl
@@ -1,10 +1,23 @@
-function wgn(length::Integer, power::Real=1.0, impedence::Real=1.0, units::String = "linear", returnComplex::Bool=false)
+#==============================================================================#
+#                                  Upsample                                    #
+#==============================================================================#
+# length        = desired length of WGN noise vector
+# power         = desired power level
+# units         = "volts", "watts", "dBm", "dBW". Case insensitive.
+# impedence     = resistance of the load
+# returnComplex = true/false. Whether or not to return complex noise 
+# 
+# Returned vector is in volts. The function uses paramter 'units' and 'impedence' to determine the RMS voltage level
+function wgn(length::Integer, power::Real=1.0, units::String = "watts", impedence::Real=1.0, returnComplex::Bool=false)
     # TODO: add argument valdiation
-    if units == "linear"
+    units = lowercase( units )
+    if units == "volts"
+        np = power^2/impedence
+    elseif units == "watts"
         np = power
-    elseif units == "dBm"
+    elseif units == "dbm"
         np = 10^((power - 30)/10)
-    elseif units == "dBW"
+    elseif units == "dbw"
         np = 10^(power/10)
     end
 
