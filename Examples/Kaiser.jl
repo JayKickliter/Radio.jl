ddb98007f66dc3db2aa93c5eb7610d7fdd149ecf
Jay Kickliter <jay@kickliter.com>
2014-04-06 12:34:52 -0600

    Added plot example

--- Examples/Kaiser.jl
+++ Examples/Kaiser.jl
@@ -1,5 +1,9 @@
+using Radio, Winston
+
 ( M, β )        = kaiserord( 0.001, 0.2*π )
 window          = kaiser( M, β )
 impulseResponse = firdes( 0.5, window )
 p               = plot_response( impulseResponse )
+
 display( p )
+file( p, "Kaiser.png" )
