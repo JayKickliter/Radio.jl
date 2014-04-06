d49d7ed4ddf1655e0e14114542cb3002a9487915
Jay Kickliter <jay@kickliter.com>
2014-04-06 13:16:51 -0600

    Refined QPSK plot.

--- Examples/QPSK.jl
+++ Examples/QPSK.jl
@@ -1,10 +1,15 @@
-using Radio
-using Winston
+using Radio, Winston
 
 symbols       = pskmod( 10000, 4 )
 noise         = wgn( length( symbols ), 10, "dBm", 1.0, true )
 signal        = symbols .+ noise
+
 constellation = scatter( real(signal), imag(signal), "." )
+setattr( constellation,
+            title = "QPSK Constellation",
+            xlabel = "In Phase",
+            ylabel = "Quadrature"
+)                
 
 display( constellation )
 file( constellation,  "QPSK.png" )
