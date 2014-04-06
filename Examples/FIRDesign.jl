326b8c8a928e0c0271d17c34b4e181b446f73015
Jay Kickliter <jay@kickliter.com>
2014-04-06 12:15:41 -0600

    Added FIRDesign.jl, a file which will contain FIR filter design examples.

---
+++ Examples/FIRDesign.jl
@@ -0,0 +1,5 @@
+( M, β )        = kaiserord( 0.001, 0.2*π )
+window          = kaiser( M, β )
+impulseResponse = firdes( 0.5, window )
+p               = plot_response( impulseResponse )
+display( p )
