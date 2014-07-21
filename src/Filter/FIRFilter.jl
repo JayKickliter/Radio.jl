71eed7472b153d439ce6e79722f3e97ba5e7d9b5
Jay Kickliter <jay@kickliter.com>
2014-07-21 16:46:41 -0600

    Added resampling functions

--- src/Filter/FIRFilter.jl
+++ src/Filter/FIRFilter.jl
@@ -1,3 +1,9 @@
+#==============================================================================#
+#               ____ _ _  _ ____ _    ____    ____ ____ ___ ____               #
+#               [__  | |\ | | __ |    |___    |__/ |__|  |  |___               #
+#               ___] | | \| |__] |___ |___    |  \ |  |  |  |___               #
+#==============================================================================#
+
 function filt{T}( taps::Vector{T}, signal::Vector{T} )
     @assert length(signal) > length(taps)
     signal_len     = length( signal )
@@ -16,6 +22,15 @@ function filt{T}( taps::Vector{T}, signal::Vector{T} )
     output_buffer
 end
 
+
+
+
+#==============================================================================#
+#                      _  _    ___ ____    ___  ____ ___                       #
+#                      |__|     |  |  |    |__] |___ |__]                      #
+#                      |  |     |  |__|    |    |    |__]                      #
+#==============================================================================#
+
 function polyize{T}( h::Vector{T}, interpolation )
     hLen         = length( h )
     tapsPerPhase = int( ceil( hLen/interpolation ))
@@ -33,6 +48,14 @@ function polyize{T}( h::Vector{T}, interpolation )
     pfb          = reshape( h, nFilters, tapsPerPhase )'
 end
 
+
+
+
+#==============================================================================#
+#               _ _  _ ___ ____ ____ ___  _    ____ ____ ___ ____              #
+#               | |\ |  |  |___ |__/ |__] |    |  | |__|  |  |___              #
+#               | | \|  |  |___ |  \ |    |___ |__| |  |  |  |___              #
+#==============================================================================#
 
 function interpolate{T}( PFB::Array{T, 2}, x::Vector{T} )
     (Ntaps, Nφ) = size( PFB )           # each column is a phase of the PFB, the rows hold the individual taps
@@ -69,4 +92,62 @@ function interpolate{T}( PFB::Array{T, 2}, x::Vector{T} )
 end
 
 interpolate( h, x, interpolation ) = polyinterpolate( polyize( h, interpolation ), x )
+
+
+
+
+#==============================================================================#
+#           ____ ____ ___     ____ ____ ____ ____ _  _ ___  _    ____          #
+#           |__/ |__|  |      |__/ |___ [__  |__| |\/| |__] |    |___          #
+#           |  \ |  |  |  .   |  \ |___ ___] |  | |  | |    |___ |___          #
+#==============================================================================#
+
+function resample{T}( PFB::Array{T, 2}, x::Vector{T}, ratio::Rational )
+    
+    (Ntaps, Nφ) = size( PFB ) # each column is a phase of the PFB, the rows hold the individual taps
+    L    = num(ratio)
+    M    = den( ratio )    
+    xLen = length( x )        # number of input items    
+    
+    xLen * L % M == 0 || error("signal length * interpolation mod decimation must be 0")    
+    
+    yLen = int(xLen*L/M)
+    y    = zeros( T, yLen )
+        
+    for m = 0:yLen-1
+        
+        φ    = mod( m*M, L)
+        nm   = int( floor( m*M / L ))
+        kMax = nm < Ntaps ? nm+1 : Ntaps
+        acc  = zero(T)
+
+        # @printf( "\n\nφ = %d", φ+1 )
+        # @printf( "\tnm = %d", nm+1 )
+        # @printf( "\tkMax = %d\n", kMax)
+        # @printf( "y[%d] =", m+1 )
+        
+        
+        for k = 0:kMax-1
+            # @printf( "\tPFB[%d, %d] * x[%d]", k+1, φ+1, nm+1-k )
+            # @printf( "\t%f * %f", PFB[k+1, φ+1], x[nm+1-k] )
+            acc += PFB[ k+1, φ+1 ] * x[ nm+1-k ]
+        end
+        
+        # @printf( "\ny[%d] = %f", m+1, acc )
+        
+        y[m+1] = acc
+    
+    end
+
+    return y
+end
+
+
+
+
+#==============================================================================#
+#                      ___  ____ ____ _ _  _ ____ ___ ____                     #
+#                      |  \ |___ |    | |\/| |__|  |  |___                     #
+#                      |__/ |___ |___ | |  | |  |  |  |___                     #
+#==============================================================================#
 
