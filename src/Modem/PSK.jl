ebe5368480ae07c0b638f64f323c3e5303e8a5a3
Jay Kickliter <jay@kickliter.com>
2015-02-22 15:23:15 -0700

    Switching to new modem scheme

--- src/Modem/PSK.jl
+++ src/Modem/PSK.jl
@@ -1,40 +1,67 @@
-#==============================================================================#
-#                         Phase Shift Keying Modulation                        #
-#==============================================================================#
-# data  = Contains the integer data you want modulated.
-#           It is up to you to ensure that 
-# 
-# 
-# 
-# 
+abstract Modem
+abstract CodingScheme
 
-function pskmod( data, modOrder::Integer, encoding::String = "", samplesPerSymbol::Integer = 1, ISIFilter::Vector = [] )
-    m = [ 0 : modOrder-1 ]
-    if modOrder == 4
-        Φ = pi/modOrder
-    else
-        Φ = 0.0
-    end
-    ideal = exp(2*pi*m*im/modOrder .+ Φ*im)
 
-    outputVec = Array(Complex128, length(data))
+type Gray <: CodingScheme end
 
-    for i in 1:length(data)
-        outputVec[i] = ideal[ data[i] + 1 ]
-    end
-    
-    if samplesPerSymbol == 1
-        return outputVec
-    end
 
-    if ISIFilter == []        
-        ISIFilter = rcos( 0.3, 10, samplesPerSymbol )
-    end    
-    
-    outputVec = interpolate( outputVec, samplesPerSymbol, ISIFilter )
+function encode( ::Type{Gray}, n::Integer )
+    n $ (n >> 1)
 end
 
-function pskmod( symbols::Integer, modOrder::Integer, samplesPerSymbol::Integer = 1, ISIFilter::Vector = [] )
-    data = rand( 0:modOrder-1, symbols )
-    pskmod( data, modOrder, "", samplesPerSymbol, ISIFilter )
+
+function decode( ::Type{Gray}, n::Integer )
+    p = n
+     while (n >>= 1) != 0
+         p $= n
+     end
+     return p
 end
+
+encode{T<:CodingScheme}( ::Type{T}, N::AbstractVector ) = [ encode( T, n) for n in N ]
+
+
+
+type PSKModem
+    modOrder::Integer                   # Modulation order, or bits per symbol. The constellation has modOrder^2 symbols
+    constellation::Vector{Complex} # ideal symbol constellation
+end
+
+function PSKModem( symbols::Integer )
+    ispow2( symbols ) || error( "symbols must be a power of 2" )
+    
+    modOrder      = log2( symbols )
+    Δ𝜙            = 2π/symbols
+    constellation = [ exp(Δ𝜙*im*decode( Gray, i)) for i in 0: symbols-1 ]
+    
+    return PSKModem( modOrder, constellation )
+end
+
+
+
+
+function modulate( modem::PSKModem, datum::Integer )
+    modem.constellation[datum+1]
+end
+
+function modulate( modem, data::AbstractVector )
+    [ modulate( modem, datum ) for datum in data ]
+end
+
+function demodulate( modem::PSKModem, datum::Integer )
+    
+end
+
+function demodulate( modem, data::AbstractVector{Integer} )
+    [ demodulate( modem, datum ) for datum in data ]
+end
+
+
+#=
+
+modem   = PSKModem( 16 )
+symbols = modulate( modem, [0:2^modem.modOrder-1])
+plot(symbols)
+
+=#
+
