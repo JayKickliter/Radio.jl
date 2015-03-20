f7763d8bc63428aa1e264e48a3d985816c6b704d
Jay Kickliter <jay@kickliter.com>
2015-03-20 16:37:44 -0600

    Switch to pyplot

--- src/scratchpad.jl
+++ src/scratchpad.jl
@@ -1,17 +1,24 @@
 import Radio
 import DSP
 
-M          = 8
+M          = 4
 modulation = Radio.PSK(M)
 modem      = Radio.Modem( modulation, samplesPerSymbol=2 )
 txData     = rand( 0:M-1, 1000 )
 symbols    = Radio.modulate( modem, txData )
 rxData     = Radio.demodulate( modem, symbols )
+taps       = modem.txFilter.h
 
-spectrum =  DSP.periodogram( symbols )
+p = DSP.welch_pgram( symbols, window=DSP.hanning )
+freqs = fftshift(DSP.freq( p ))
+power = log10( fftshift(DSP.power( p )) )
 
 
+using PyPlot
 
-using Winston
-p = plot(fftshift(log10(abs2(fft(symbols)))))
-display( p )
+clf()
+subplot( 2,1,1 )
+scatter( real(symbols), imag(symbols) )
+
+subplot( 2,1,2 )
+plot( freqs, power )
